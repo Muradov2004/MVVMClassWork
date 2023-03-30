03dd98a75d7d26637f2a0bb8a37ce4221e7c12fb
Ali <112820884+Muradov2004@users.noreply.github.com>
2023-03-31 01:16:31 +0400

    Add project files.

---
+++ MVVMClassWork/View/MainView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using MVVMClassWork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace MVVMClassWork.View
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        private List<Person> persons = new();
+        public MainView()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtFirstName.Text != string.Empty && txtLastName.Text != string.Empty)
+            {
+                persons.Add(new() { FirstName = txtFirstName.Text, LastName = txtLastName.Text });
+                listView.Items.Add(new { FirstName = txtFirstName.Text, LastName = txtLastName.Text });
+                txtFirstName.Clear();
+                txtLastName.Clear();
+            }
+            else
+                MessageBox.Show("You can not enter empty name or surname", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            InfoWindow infoWindow = new();
+            infoWindow.FirstNameBlock.Text = "FirstName : " + persons[listView.SelectedIndex].FirstName;
+            infoWindow.LastNameBlock.Text = "LastName : " + persons[listView.SelectedIndex].LastName;
+            infoWindow.Show();
+        }
+    }
+}
---
+++ MVVMClassWork/ViewModels/PersonViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MVVMClassWork.Models;
+using System.ComponentModel;
+
+namespace MVVMClassWork.ViewModels
+{
+    internal class PersonViewModel : INotifyPropertyChanged
+    {
+		private Person? _person;
+
+        public Person? Person
+		{
+			get { return _person; }
+			set 
+			{
+				_person = value;
+				OnPropertyChanged(nameof(Person));
+			}
+		}
+		public PersonViewModel() => Person = new Person();    
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+    }
+}
